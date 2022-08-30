6e9eeaff06679a728e5f10ba518f0dcab18469c8
Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>
2022-08-30 17:11:10 +0700

    Update Set-JumboFrame.ps1

--- Set-JumboFrame.ps1
+++ Set-JumboFrame.ps1
@@ -1,1 +1,5 @@
-# test
+$StorageAdapters = @('SLOT 3 PORT 1', 'SLOT 3 PORT 2')
+foreach ($port in $StorageAdapters)
+{
+  Set-NetAdapterAdvancedProperty -Name $port -RegistryKeyword “*JumboPacket” -Registryvalue 9014
+}
