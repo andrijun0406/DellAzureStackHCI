18fe397114a612a595b9ebbc57410e44506bf1de
Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>
2022-08-30 19:53:13 +0700

    Update Get-DellDeviceDriver.ps1

--- Get-DellDeviceDriver.ps1
+++ Get-DellDeviceDriver.ps1
@@ -1,1 +1,2 @@
-# test
+# Get list of device driver relevant to Azure Stack HCI OS
+Get-PnpDevice | Select-Object Name, @{l='DriverVersion';e={(Get-PnpDeviceProperty -InstanceId $_.InstanceId -KeyName 'DEVPKEY_Device_DriverVersion').Data}} -Unique | Where-Object {($_.Name -like "*HBA*") -or ($_.Name -like "*mellanox*") -or ($_.Name -like "*Qlogic*") -or ($_.Name -like "*X710*") -or ($_.Name -like "*Broadcom*") -or ($_.Name -like "*marvell*") }
