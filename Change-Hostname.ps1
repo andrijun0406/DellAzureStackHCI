3c8ad2ccf7d51d868ac78fca4c2b7f3a448ec10c
Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>
2022-08-31 09:47:51 +0700

    Create Change-Hostname.ps1

---
+++ Change-Hostname.ps1
@@ -0,0 +1,4 @@
+Rename-Computer -NewName hcinprdhst001 -Restart
+#Rename-Computer -NewName hcinprdhst002 -Restart
+#Rename-Computer -NewName hcinprdhst003 -Restart
+#Rename-Computer -NewName hcinprdhst004 -Restart
