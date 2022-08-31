0a1e20d021df23796c8358e39efb06213dcd2670
Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>
2022-08-31 14:31:35 +0700

    Update Join-Domain.ps1

--- Join-Domain.ps1
+++ Join-Domain.ps1
@@ -1,12 +1,23 @@
 #at this stage, servers can not resolved to DNS yet so use IP address as Computer Name
 #also use local administrator to connect.
 #adjust the following parameter accordingly and run below command per remote powershell cli windows
+#if you running this from management or Windows Admin Centre (WAC) hosts then you need to setup WinRM TrustedHost manually first as the following
 
 $server1 = "10.189.192.62"
 $server2 = "10.189.192.63"
 $server3 = "10.189.192.64"
 $server4 = "10.189.192.65"
 
+Set-Item WSMAN:\Localhost\Client\TrustedHosts -Value $server1 -Force
+Set-Item WSMAN:\Localhost\Client\TrustedHosts -Value $server2 -Force
+Set-Item WSMAN:\Localhost\Client\TrustedHosts -Value $server3 -Force
+Set-Item WSMAN:\Localhost\Client\TrustedHosts -Value $server4 -Force
+
+#view TrustedHost List:
+Get-Item WSMAN:\Localhost\Client\TrustedHosts
+
+#after joined domain clear the list and use kerberos or Group Policy CredSSP
+#Clear-Item WSMAN:\Localhost\Client\TrustedHost
 
 # enter remote powershell command in each Powershell cli windows
 $user="$server1\Administrator"
@@ -24,5 +35,6 @@ Enter-PSSession -ComputerName $server1 -Credential $user
 #$user="$server4\Administrator"
 #Enter-PSSession -ComputerName $server4 -Credential $user
 
-#Add-Computer -NewName "hcinprdhst001" -DomainName "contoso.com" -Credential "Contoso\ADAdmin" -Restart -Force
+#Run the following command after successfully enter a remote powershell session
+#Add-Computer -DomainName "contoso.com" -Credential "Contoso\ADAdmin" -Restart -Force
 #Add-LocalGroupMember -Group "Administrators" -Member "king@contoso.local"
