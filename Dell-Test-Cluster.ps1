0dbd278523d048185427210bd6a9fb53485123c9
Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>
2022-08-31 16:32:33 +0700

    Create Dell-Test-Cluster.ps1

---
+++ Dell-Test-Cluster.ps1
@@ -0,0 +1,17 @@
+$ServerList = "HCINPRDHST001", "HCINPRDHST002", "HCINPRDHST003", "HCINPRDHST004"
+$ClusterName = "HCINPRDCLU001"
+$ClusterIPAddress ="10.189.192.66"
+
+#Disable USB NIC used by iDRAC to communicate to host just for test-cluster (only applies to physical servers)
+$USBNics=get-netadapter -CimSession $ServerList -InterfaceDescription "Remote NDIS Compatible Device" -ErrorAction Ignore
+if ($USBNics){
+    Disable-NetAdapter -CimSession $ServerList -InterfaceDescription "Remote NDIS Compatible Device" -Confirm:0
+}
+
+#Test cluster first
+Test-Cluster -Node $ServerList -Include "Storage Spaces Direct","Inventory","Network","System Configuration","Hyper-V Configuration"
+
+#Enable USB NICs again
+if ($USBNics){
+    Enable-NetAdapter -CimSession $Servers -InterfaceDescription "Remote NDIS Compatible Device" -Confirm:0
+}
