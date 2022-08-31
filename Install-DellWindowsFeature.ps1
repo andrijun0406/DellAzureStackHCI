ae53058a7f7e3f19e3c30a364676f666afa2f674
Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>
2022-08-31 09:38:52 +0700

    Update Install-DellWindowsFeature.ps1

--- Install-DellWindowsFeature.ps1
+++ Install-DellWindowsFeature.ps1
@@ -1,1 +1,11 @@
-# test
+#define and install features
+
+# Fill in these variables with your values
+$ServerList = "hcinprdhst001", "hcinprdhst002", "hcinprdhst003", "hcinprdhst004"
+$FeatureList = "BitLocker", "Data-Center-Bridging", "Failover-Clustering", "FS-FileServer", "FS-Data-Deduplication", "Hyper-V", "Hyper-V-PowerShell", "RSAT-AD-Powershell", "RSAT-Clustering-PowerShell", "NetworkATC", "Storage-Replica"
+
+# This part runs the Install-WindowsFeature cmdlet on all servers in $ServerList, passing the list of features in $FeatureList.
+Invoke-Command ($ServerList) {
+    Install-WindowsFeature -Name $Using:Featurelist -IncludeAllSubFeature -IncludeManagementTools
+}
+
