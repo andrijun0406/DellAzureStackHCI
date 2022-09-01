1fea4b5cd86206d8ad6f8bc73630ecb6ee89f624
Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>
2022-09-01 21:37:26 +0700

    Create Disable-Rndis.ps1

---
+++ Disable-Rndis.ps1
@@ -0,0 +1,7 @@
+#Rename and Configure USB NICs
+$USBNics=get-netadapter -CimSession $Servers -InterfaceDescription "Remote NDIS Compatible Device" -ErrorAction Ignore
+if ($USBNics){
+    $Network=(Get-ClusterNetworkInterface -Cluster $ClusterName | Where-Object Adapter -eq "Remote NDIS Compatible Device").Network | Select-Object -Unique
+    $Network.Name="iDRAC"
+    $Network.Role="none"
+}
