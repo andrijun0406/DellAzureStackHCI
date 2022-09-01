810b1509bfaffad7fd1783e9eca995bd76e0a54c
Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>
2022-09-01 21:38:44 +0700

    Update Disable-Rndis.ps1

--- Disable-Rndis.ps1
+++ Disable-Rndis.ps1
@@ -1,3 +1,6 @@
+$Servers = "HCINPRDHST001", "HCINPRDHST002", "HCINPRDHST003", "HCINPRDHST004"
+$ClusterName = "HCINPRDCLU001"
+
 #Rename and Configure USB NICs
 $USBNics=get-netadapter -CimSession $Servers -InterfaceDescription "Remote NDIS Compatible Device" -ErrorAction Ignore
 if ($USBNics){
