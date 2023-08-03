71c27a52ac67729724e422cd5769bcf8839fbe08
Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>
2023-08-03 21:28:51 +0700

    Update and rename Change-Hostname.ps1 to 05_Change-Hostname.ps1
    Signed-off-by: Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>

---
+++ Deployment/05_Change-Hostname.ps1
@@ -0,0 +1,4 @@
+Rename-Computer -NewName AxNode1 -Restart
+#Rename-Computer -NewName AxNode2 -Restart
+#Rename-Computer -NewName AxNode3 -Restart
+#Rename-Computer -NewName AxNode4 -Restart
--- Deployment/Change-Hostname.ps1
+++
@@ -1,4 +0,0 @@
-Rename-Computer -NewName hcinprdhst001 -Restart
-#Rename-Computer -NewName hcinprdhst002 -Restart
-#Rename-Computer -NewName hcinprdhst003 -Restart
-#Rename-Computer -NewName hcinprdhst004 -Restart
