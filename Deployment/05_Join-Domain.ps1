9b3aa29c81939ef5844d255ee43b2adcc070e2c7
Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>
2023-08-04 11:09:36 +0700

    Update 05_Join-Domain.ps1
    Signed-off-by: Andri Junardi Ahmad <49418635+andrijun0406@users.noreply.github.com>

--- Deployment/05_Join-Domain.ps1
+++ Deployment/05_Join-Domain.ps1
@@ -6,10 +6,10 @@ $domainname = "contoso.com"
 $oupath = "OU=AZHCI,DC=CONTOSO,DC=COM"
 $newname = "AxNode1"
 
+Add-Computer -DomainName $domainname -OUPath $oupath -NewName $newname -Credential $credential -Restart -Force
+
 # add your admin user here:
 $domainadminuser1 = "john@contoso.com"
 $domainadminuser2 = "bob@contoso.com"
-
-Add-Computer -DomainName $domainname -OUPath $oupath -NewName $newname -Credential $credential -Restart -Force
 Add-LocalGroupMember -Group "Administrators" -Member $domainadminuser1
 Add-LocalGroupMember -Group "Administrators" -Member $domainadminuser2
